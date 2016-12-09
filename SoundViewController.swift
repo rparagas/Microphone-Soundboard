dd3dda2304238b169b7e67555c5bd1218f2e781a
rparagas <rayparagas@rays-air.home>
2016-12-09 19:18:40 +1100

    added view controller elements and created sound cocoa touch class (linked to storyboard)

---
+++ SoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundViewController.swift
+//  Microphone-Soundboard
+//
+//  Created by Ray Paragas on 9/12/16.
+//  Copyright © 2016 Ray Paragas. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var soundTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
