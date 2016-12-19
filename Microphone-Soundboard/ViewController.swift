ff10e5ffd3859b0be88f57ad9a3bcea8ceb97a32
Ray Paragas <rayy.paragas@gmail.com>
2016-12-19 13:15:23 +1100

    can record sounds and save into core data, can playback to test sound before saving, can display saved sounds on table view, can play sounds when selected in view table, minor bug on actual devices (saved sounds don't play initially when selected upon first loading - must select '+' button and go back to tableview screen in order to play sounds)

--- Microphone-Soundboard/ViewController.swift
+++ Microphone-Soundboard/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var soundsTableView: UITableView!
+    var audioPlayer : AVAudioPlayer?
+    var sounds : [Sound] = []
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        soundsTableView.delegate = self
+        soundsTableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            sounds = try context.fetch(Sound.fetchRequest())
+            soundsTableView.reloadData()
+        } catch {
+            print("error")
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let sound = sounds[indexPath.row]
+        cell.textLabel?.text = sound.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sound = sounds[indexPath.row]
+        do {
+            audioPlayer = try AVAudioPlayer(data: sound.audio! as Data)
+            audioPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
+    
+    
 }
 
--- SoundViewController.swift
+++ SoundViewController.swift
@@ -7,29 +7,88 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     @IBOutlet weak var soundTextField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupRecorder() {
+        do{
+            // create an audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // create URL for the audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+            // create settings for audio recorder
+            var settings : [String:Any] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+            settings[AVSampleRateKey] = 44100.0
+            settings[AVNumberOfChannelsKey] = 2
+            
+            // create a new audiorecorder
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
     }
     
     @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording {
+            //Stop the recording
+            audioRecorder?.stop()
+            
+            //Change button title to Record
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+        } else {
+            // Start the recording
+            audioRecorder?.record()
+            
+            //CHange button title to stop
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
-
+    
     @IBAction func playTapped(_ sender: Any) {
+        do {
+           try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+           audioPlayer!.play()
+        } catch {
+            print("error")
+        }
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sound = Sound(context: context)
+        sound.name = soundTextField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
     }
-
 }
